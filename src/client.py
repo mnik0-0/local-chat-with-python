48bf9db2fcf46104a7e9001a435003854c512d11
mnik0_0 <nikich060604@gmail.com>
2020-03-26 14:21:00 +0300

    отправка данных серверу и обратно

--- src/client.py
+++ src/client.py
@@ -1,16 +1,53 @@
 import socket
 import pickle
-import select
+import threading
 
 # Задача основных значений
 PORT = 9090  # Порт соединения с сервером
 IP = socket.gethostname()  # Получение локального IP
+DATA_LENGTH = 10  # Задаёт максиальное кол-во байт для передачи
+
+
+# Отправка данных серверу
+def send_data(data):
+    data = pickle.dumps(data)  # Первод данных в байты
+    data = bytes(f"{len(data):<{DATA_LENGTH}}", "utf-8") + data  # Добавление длинны данных
+    client_socket.send(data)  # Отпрвака данных серверу
+
+
+# Получение данных от сервера
+def get_data():
+    # Если данные не были получены, то вернем False
+    try:
+        len_message = int(client_socket.recv(DATA_LENGTH).decode("utf-8"))  # Получаем длину данных
+        data = client_socket.recv(len_message)  # Получаем данные заданной длинны
+        data = pickle.loads(data)  # Преобразуем данные из байт обратно в объект
+        return data
+    except:
+        return False
+
+
+# Получение данных
+def output_thread():
+    while True:
+        data = get_data()
+        if data:
+            print(data)
+
+
+# Ввод
+def input_thread():
+    while True:
+        data = input()
+        if data:
+            send_data(data)
+
 
 # Подключение к серверу
 client_socket = socket.socket()  # Получаем сокет клиента
 client_socket.connect((IP, PORT))  # Подключаемся к серверу с IP, PORT
 client_socket.setblocking(False)  # Non-blocking
 
-# Чтобы клиент не закрывался
-while True:
-    pass
+# Поток для обработки ввода и получения сообщений
+threading.Thread(target=output_thread).start()  # Получение данных
+threading.Thread(target=input_thread).start()  # Ввод
--- src/server.py
+++ src/server.py
@@ -1,10 +1,12 @@
 import socket
 import pickle
+import threading
 import select
 
 # Задача основных значений
 PORT = 9090  # Порт соединения с сервером
 IP = socket.gethostname()  # Получение локального IP
+DATA_LENGTH = 10  # Задаёт максиальное кол-во байт для передачи
 
 
 # Создание класса клиента
@@ -15,6 +17,38 @@ class Client:
         # Инициализация параметров
         self.socket = socket_  # Сокет клиента
         self.address = address  # Адрес клиента
+
+    # Отправка данных серверу
+    def send_data(self, data):
+        data = pickle.dumps(data)  # Первод данных в байты
+        data = bytes(f"{len(data):<{DATA_LENGTH}}", "utf-8") + data  # Добавление длинны данных
+        self.socket.send(data)  # Отпрвака данных серверу
+
+    # Получение данных от сервера
+    def get_data(self):
+        # Если данные не были получены, то вернем False
+        try:
+            len_message = int(self.socket.recv(DATA_LENGTH).decode("utf-8"))  # Получаем длину данных
+            data = self.socket.recv(len_message)  # Получаем данные заданной длинны
+            data = pickle.loads(data)  # Преобразуем данные из байт обратно в объект
+            return data
+        except:
+            return False
+
+
+# Получение данных
+def output_thread():
+    while True:
+        data = client.get_data()
+        if data:
+            print(data)
+
+
+# Ввод
+def input_thread():
+    while True:
+        data = input()
+        client.send_data(data)
 
 
 # Настройка сервера
@@ -29,5 +63,6 @@ client = Client(client_socket, client_address)  # Создаем нового п
 print("Клиент подключён")
 
 # Чтобы сервер не закрывался
-while True:
-    pass
+
+threading.Thread(target=output_thread).start()  # Получение данных
+threading.Thread(target=input_thread).start()  # Ввод
