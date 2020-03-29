e90eff51468669878ae04431332602f9edd693e9
mnik0_0 <nikich060604@gmail.com>
2020-03-29 16:09:34 +0300

    несколько подключений, очередь подключений

--- src/client.py
+++ src/client.py
@@ -6,6 +6,8 @@ import threading
 PORT = 9090  # Порт соединения с сервером
 IP = socket.gethostname()  # Получение локального IP
 DATA_LENGTH = 10  # Задаёт максиальное кол-во байт для передачи
+LOGGED_IN = False  # Ввёл пользователь имя или нет
+INPUT = True  # Можно ли пользователю отправлять сообщения или нет
 
 
 # Отправка данных серверу
@@ -29,17 +31,27 @@ def get_data():
 
 # Получение данных
 def output_thread():
+    global LOGGED_IN
+    global INPUT
     while True:
         data = get_data()
+        if not LOGGED_IN and not data:  # Елси до пользователя еще не дошла очередь
+            print("Подождите пока придет ваша очередь")
+            INPUT = False  # Запрещаем ему отправлять сообщения
+            while not data:  # До тех пор пока до нас не дойтёт очередь
+                data = get_data()
         if data:
+            LOGGED_IN = True  # Пользователь вошёл
+            INPUT = True  # Разрешаем отправлять
             print(data)
 
 
 # Ввод
 def input_thread():
+    global INPUT
     while True:
         data = input()
-        if data:
+        if data and INPUT:
             send_data(data)
 
 
@@ -49,5 +61,5 @@ client_socket.connect((IP, PORT))  # Подключаемся к серверу 
 client_socket.setblocking(False)  # Non-blocking
 
 # Поток для обработки ввода и получения сообщений
-threading.Thread(target=output_thread).start()  # Получение данных
 threading.Thread(target=input_thread).start()  # Ввод
+threading.Thread(target=output_thread).start()  # Получение данных
--- src/server.py
+++ src/server.py
@@ -17,14 +17,25 @@ class Client:
         # Инициализация параметров
         self.socket = socket_  # Сокет клиента
         self.address = address  # Адрес клиента
+        self.name = False  # Имя не задаём
 
-    # Отправка данных серверу
+    # Задаёт имя пользователю
+    def set_name(self):
+        self.name = self.get_data()
+
+    # Отправка данных клиенту
     def send_data(self, data):
         data = pickle.dumps(data)  # Первод данных в байты
         data = bytes(f"{len(data):<{DATA_LENGTH}}", "utf-8") + data  # Добавление длинны данных
         self.socket.send(data)  # Отпрвака данных серверу
 
-    # Получение данных от сервера
+    # Отправляет сообщение всем пользователям, кроме отправителя
+    def send_to_all(self, data, to_clients):
+        for client_socket, client in to_clients.items():  # Перебор всех пользоваетелей
+            if client_socket != self.socket:  # Кроме нашего
+                client.send_data(data)
+
+    # Получение данных от клиента
     def get_data(self):
         # Если данные не были получены, то вернем False
         try:
@@ -39,16 +50,47 @@ class Client:
 # Получение данных
 def output_thread():
     while True:
-        data = client.get_data()
-        if data:
-            print(data)
+        sockets, _, _ = select.select(socket_list, [], [])  # Прослушиваем все подключения
+        for i_socket in sockets:  # Пробегаемся по ним
+            if i_socket == server_socket:  # Если такого подключения еще нет
+                # Принимаем подключение
+                client_socket, client_address = server_socket.accept()  # Принимаем подключение
+                client = Client(client_socket, client_address)  # Создаем нового пользователя
+
+                clients.update({client.socket: client})  # Добавляем в список пользоваетелей
+                socket_list.append(client.socket)  # Добавляем сокет пользователя
+                print(f"Новый пользователь присоединился {client.address[1]}")
+
+                if not client.name:  # Если его имя не задано
+
+                    client.send_data("Введите своё имя")
+                    client.set_name()  # Устанавливаем ему имя
+
+                    # Оповещаем о подключении
+                    print(f"Пользователь {client.address[1]} выбрал имя {client.name}")
+                    client.send_to_all(f"Пользователь {client.name} присоеденился", clients)
+                    continue
+
+            else:  # Если такое подключение уже существует
+                client = clients[i_socket]  # Получаем пользователя
+
+                # Получаем и отправляем сообщения
+                data = client.get_data()
+                if not data:
+                    socket_list.remove(client.socket)
+                    clients.pop(client.socket)
+                    print(f"Пользователь {client.name} вышел")
+                else:
+                    client.send_to_all(f"{client.name} {client.address[1]} >>> {data}", clients)
 
 
 # Ввод
 def input_thread():
     while True:
         data = input()
-        client.send_data(data)
+        if data:
+            for client in clients.values():
+                client.send_data(f"server >>> {data}")
 
 
 # Настройка сервера
@@ -56,13 +98,10 @@ server_socket = socket.socket()  # Получаем сокет сервера
 server_socket.bind((IP, PORT))  # Настраиваем подключение через IP, PORT
 server_socket.listen(5)  # Задаём максимальное число подключений
 
-# Принимаем подключение
-client_socket, client_address = server_socket.accept()  # Принимаем подключение
-client = Client(client_socket, client_address)  # Создаем нового пользователя
-
-print("Клиент подключён")
+# Для работы с клиентами
+clients = dict()
+socket_list = [server_socket]
 
 # Чтобы сервер не закрывался
-
 threading.Thread(target=output_thread).start()  # Получение данных
 threading.Thread(target=input_thread).start()  # Ввод
